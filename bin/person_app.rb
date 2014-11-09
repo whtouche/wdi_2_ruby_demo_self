5b0047f200df4822a286c86c4a75a451a6401c0b
Ben White <whtouche@gmail.com>
2014-11-09 14:40:25 -0500

    cleanup

--- bin/person_app.rb
+++ bin/person_app.rb
@@ -1,14 +1,26 @@
+require 'pry'
 require_relative '../lib/person'
 
 joe = Person.new('Joe', 'Smoe')
 
 # change joe's status to active
-joe.status = 'active'
+# joe.status = 'active'
 
 # change  joe's status to inactive
-joe.status = 'inactive'
+# joe.status = 'inactive'
 
 # Ok, joe is signed the contract and we make him active again.
-joe.signed_contract
+# joe.signed_contract
 
+# joe.all_people
 
+# Creating a person four times and then
+4.times do |i|
+  Person.all_people << Person.new("person_first_#{i}", "person_last_#{i}")
+end
+
+puts "All my people = #{Person.all_people.inspect}"
+
+puts "Count is #{Person.count}"
+
+# binding.pry
--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,24 @@
+require 'pry'
 # Define a Person class
 class Person
+  @@count = 0
+
+  # Class variable
+  @@all_people = []
+
+  def Person.count
+    @count
+  end
+
+  def Person.count=(new_count)
+    @count = new_count
+  end
+
+  # Class method
+  # start method with self.
+  def self.all_people
+    @@all_people
+  end
 
   attr_reader :first_name, :status
   attr_accessor :last_name
@@ -7,7 +26,15 @@ class Person
   def initialize(fname, lname)
     @first_name, @last_name = fname, lname
     @status = 'inactive'
-  end 
+
+    # Increment the Person counter
+    # Where Person counter is a class variable
+
+
+    # Below: self = joe
+    # @@all_people << self
+    Person.count = Person.count + 1
+  end
 
   def full_name
     "#{@first_name} #{@last_name}"
@@ -16,12 +43,20 @@ class Person
   def status=(new_status)
     @status = new_status
 
+    # Notify the insurance agent
+    # Email or ping the agent
+
     # Side Effect of changing a Person's status is to say it
-    %x{ say "#{full_name} has changed their status to #{status}"}
+    # %x {} invokes a unix command (in this instance, say)
+    puts "#{full_name} has changed their status to #{status}"
+  end
+
+  def croaked
+    self.status = 'reach their expiration date'
   end
 
   def signed_contract
-    # Problem: 
+    # Problem:
     # Will not call the setter method to and notify, or say, that the status has
     # changed!
     # @status = 'active'
@@ -34,4 +69,5 @@ class Person
     # This will notify, or say, that theh Person's status has changed.
     self.status = 'active'
   end
-end       
+  # binding.pry
+end
